a75c1ee6059dde380ce464fe75364d0df103d3e8
George Soulantikas <soulgeo01@gmail.com>
2026-04-02 13:34:54 +0300

    home page update

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,19 +1,15 @@
+import { Link } from "react-router"
 import Layout from "../layout/Layout"
-import CodeEditor from "../ui/CodeEditor"
 
 function HomePage() {
   return (
     <Layout>
-      <div className="flex flex-row w-full text-xl font-bold p-4">
-        <h1>Lorem Ipsum</h1>
-      </div>
-      <div className="flex flex-col md:flex-row w-full">
-        <div className="w-full flex-1 p-4">
-          <h2 className="font-bold">Exercise</h2>
-          <p>Lorem ipsum dolor sit amet</p>
-        </div>
-        <div className="w-full md:w-32 flex-1 p-4">
-          <CodeEditor />
+      <div className="flex flex-col h-screen w-full items-center justify-center gap-4">
+        <h2 className="text-4xl">Your trusty python tutor.</h2>
+        <h1 className="text-xl">Learn python through interactive lessons and coding exercises.</h1>
+        <div className="flex flex-row gap-4 mt-2">
+          <Link to="/" className="btn btn-primary">Sign In</Link>
+          <Link to="/" className="btn btn-secondary">Create an Account</Link>
         </div>
       </div>
     </Layout>
